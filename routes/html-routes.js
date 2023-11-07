5490b5f1f302e765e2fd9f4b325ac6558c65fd27
Cristian <139021004+Cbaca4@users.noreply.github.com>
2023-11-06 23:57:44 -0600

    added html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const path = require('path');
-
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../'))
-})
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+router.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+});
